a3ec85f691e21fe16d910fea585fa8a9f37b659c
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-11-14 13:50:34 +0700

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Ricky Primayuda Putra on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
